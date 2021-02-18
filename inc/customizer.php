44159b485d9ab7d78e9a6c2aacbc9a372c9ed640
Isabella Chen <miss.izzyc@gmail.com>
2021-02-18 11:57:04 +0100

    update readme

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function steppenwolf_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function steppenwolf_customize_preview_js() {
-	wp_enqueue_script( 'steppenwolf-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'steppenwolf-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), STEPPENWOLF_VERSION, true );
 }
 add_action( 'customize_preview_init', 'steppenwolf_customize_preview_js' );
