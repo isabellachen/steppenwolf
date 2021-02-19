2704350eb614485b95aa35ddcfd93c9cc6a28874
Isabella Chen <miss.izzyc@gmail.com>
2021-02-19 12:54:09 +0100

    mobile menu animations

--- js/navigation.js
+++ js/navigation.js
@@ -32,11 +32,26 @@
   });
 
   $topLevelMenuItem.click(function () {
+    if (event.target !== this) {
+      return;
+    }
     const self = this; // the parent li that is clicked, e.g. About Us
     const target = $(self).find('.level-0'); // the ul submenu under the li e.g. About Us > Our Story
+    $(this).find('.level-1').removeClass('open'); //the sub-submenus are always closed when openining a top level menu
     target.toggleClass('open'); // open and close the submenu of the clicked parent li
 
     $topLevelSubMenu.not(target).each(function () {
+      $(this).removeClass('open'); // close all other submenus
+      $(this).find('.level-1').removeClass('open'); // close the sub-submenus
+    });
+  });
+
+  $secondLevelMenuItem.click(function () {
+    const self = this;
+    const target = $(self).find('.level-1');
+    target.toggleClass('open');
+
+    $secondLevelSubMenu.not(target).each(function () {
       $(this).removeClass('open'); // close all other submenus
     });
   });
